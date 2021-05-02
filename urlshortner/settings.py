23f7634496c99ee586edba329cbd6410079bcd56
nikita awasarmol <57043120+nikitaawasarmol@users.noreply.github.com>
2021-05-02 07:43:18 +0530

    Update settings.py

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c$7u4n8-1$nxv+22!@+tj@j^)50mpu7pm14ne-sv)gwx+!#wsi'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
